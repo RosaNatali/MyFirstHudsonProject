55fabc5703bf6496abfd0ed901df470a9a743ba0
SARSOURA <SARSOURA@SARSOURAENA>
2017-10-12 15:14:27 -0700

    jh

---
+++ HelloGitHudson/src/com/esprit/tn/Main.java
@@ -0,0 +1,12 @@
+package com.esprit.tn;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
